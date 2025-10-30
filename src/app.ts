14318fa0bced3771c20b863ddae519790e0e472b
harsh-krishang <krishangtechnolab@gmail.com>
2025-10-30 19:01:28 +0530

    cors

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,31 @@ import express from 'express';
 import apiRouter from './routes';
 import { errorHandler } from './middleware';
 import 'module-alias/register';
+import cors from 'cors';
 
 const app = express();
 
-// Middleware
+const allowedOrigins = [
+  'http://localhost:3000', // local dev
+  'https://filantro-frontend.vercel.app', // production frontend
+];
+
+const corsOptions: cors.CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      // Allow requests with no origin (like mobile apps or curl)
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  // credentials: true, // Allow cookies and auth headers
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 // health route
