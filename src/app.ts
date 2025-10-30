5aa4899f9b4baad33f3da9398c7aeb4eefd53911
harsh-krishang <krishangtechnolab@gmail.com>
2025-10-30 10:48:46 +0530

    added router the correct way

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import apiRouter from './routes';
 
 const app = express();
 
@@ -8,6 +9,13 @@ app.use(express.json());
 // Test route
 app.get('/health', (req, res) => {
   res.send('Simple backend is running!');
+});
+
+app.use('/api/v1', apiRouter);
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
 });
 
 export default app;
