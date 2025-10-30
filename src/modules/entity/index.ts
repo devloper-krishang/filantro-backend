c0477a6e95936fcc2914e143b0325e60e20231d0
harsh-krishang <krishangtechnolab@gmail.com>
2025-10-30 16:58:41 +0530

    fix

--- src/modules/entity/index.ts
+++ src/modules/entity/index.ts
@@ -1,5 +1,5 @@
 export * from './model/entity.model';
 export * from './service/entity.service';
 export * from './schemas/entity.schemas';
-export * from './routes';
+// export * from './routes/entity.routes';
 export * from './controllers/entity.controller';
